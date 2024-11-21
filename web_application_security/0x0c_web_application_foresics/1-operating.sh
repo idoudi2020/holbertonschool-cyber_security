6d3bef64197f295ef90c587920e41f31ccca105b
idoudi2020 <idoudimokhtar@gmail.com>
2024-11-21 08:13:30 -0600

    task0

--- web_application_security/0x0c_web_application_foresics/1-operating.sh
+++ web_application_security/0x0c_web_application_foresics/1-operating.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# File to analyze
-log_file="dmesg.txt"
-
-# Extract the Linux version line from the file
-if [ -f "$log_file" ]; then
-    grep -i "Linux version" "$log_file" | head -n 1
-else
-    echo "Error: File '$log_file' not found."
-fi
-
+grep "Linux version" dmesg
